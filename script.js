751c77edfb411933650fe8ef662d642619c73c13
ATCO Dias <diaslasd@gmail.com>
2022-12-16 13:32:02 -0300

    🚀 media play

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let urlAlura = 'https://cursos.alura.com.br/dashboard';
 
 function getRank(urlOrigem, login) {
     const tiposRank = ["day", "full", "monthly", "weekly"];
-    tiposRank.forEach( function(tipo){
-        let url = `${urlOrigem}/${(tipo == 'day')? '' : 'rank/' + tipo}`
+    tiposRank.forEach(function (tipo) {
+        let url = `${urlOrigem}/${(tipo == 'day') ? '' : 'rank/' + tipo}`
         fetch(url)
             .then(res => res.text())
             .then(text => {
@@ -16,31 +16,27 @@ function getRank(urlOrigem, login) {
                     let hoje = fragment
                         .querySelector('.pointsGrid-cell:last-child > span > strong')
                         .innerText;
-                    let hojeFormatado =  new Intl.NumberFormat('pt-BR').format(parseInt( hoje ));
+                    let hojeFormatado = (hoje) ? new Intl.NumberFormat('pt-BR').format(parseInt(hoje)) : 0;
                     let pontos = fragment
                         .querySelectorAll('.pointsGrid-cell--high-score > span > strong');
                     let media = calcularMedia(pontos);
-                    // let total = fragment
-                    //     .querySelector('.profile-info-item-points > strong')
-                    //     .innerText;
-                    document.getElementById(tipo).innerText = hojeFormatado + " pontos";
+                    document.getElementById(tipo).innerText = hojeFormatado;
                     document.getElementById('media').innerText = media;
-                    // document.getElementById('total').innerText = total.replace(/(\d{3})?(\d{3})?/, "$1.$2");
                 } else {
                     let ranking = fragment
                         .querySelector('.rankPage-position-myself > strong')
                         .innerText
-                        .replace(/[\r\n ]/g, ''); 
+                        .replace(/[\r\n ]/g, '');
                     let pontos = fragment
                         .querySelector('.rankPage-position-myself > div > span:last-child > strong')
                         .innerText
                         .replace(/[\r\n ]/g, '')
-                        .replace(',','.'); 
+                        .replace(',', '.');
                     document.getElementById(tipo).innerHTML = `<a href="https://cursos.alura.com.br/user/${login}/rank/${tipo}" target="_blank">${pontos} (${ranking})</a>`;
                 }
             })
-            .catch(function(err) {
-                console.info(err);                
+            .catch(function (err) {
+                console.info(err);
             });
     });
 }
@@ -48,15 +44,35 @@ function getRank(urlOrigem, login) {
 function calcularMedia(pontos) {
     let soma = 0;
     let total = pontos.length;
-    pontos.forEach( ponto => {
-        soma += parseInt(ponto.innerText);
-        //console.log(ponto);
-    });
-    let numeroFormatado = new Intl.NumberFormat('pt-BR').format(Math.round( soma/ total ));
+    pontos.forEach(ponto => soma += parseInt(ponto.innerText));
+    let numeroFormatado = new Intl.NumberFormat('pt-BR').format(Math.round(soma / total));
     return numeroFormatado;
 }
 
-function getUser(url) { 
+function Continuar() {
+    let url = 'https://cursos.alura.com.br/dashboard';
+    let botaoContinuar = document.getElementById("continuar");
+    fetch(url)
+        .then(res => res.text())
+        .then(text => {
+            let fragment = document.createElement('div');
+            fragment.innerHTML = text;
+            return fragment;
+        })
+        .then(fragment => {
+            let link = 'https://cursos.alura.com.br/';
+            let curso = fragment.querySelector('.big-card__button').href;
+            let cursoPosicao = curso.indexOf('course');
+            curso = ( cursoPosicao != -1)? curso.substring(cursoPosicao) : '';
+            link += curso;
+            botaoContinuar.addEventListener('click', () => window.open(link, '_blank'));
+        })
+        .catch(function (err) {
+            console.info(err);
+        });
+}
+
+function getUser(url) {
     fetch(url)
         .then(res => res.text())
         .then(text => {
@@ -71,9 +87,10 @@ function getUser(url) {
                 .querySelectorAll('.profile-info-name')[1].innerText;
             document.getElementById('usuario').innerText = usuario;
             document.getElementById('link').href = `https://cursos.alura.com.br/user/${login}`;
-            getRank(`https://cursos.alura.com.br/user/${login}`, login);            
+            Continuar();
+            getRank(`https://cursos.alura.com.br/user/${login}`, login);
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.info(err);
             document.getElementById('login').innerHTML = '<a href="https://cursos.alura.com.br/" target="_blank" id="link">Login Alura</a>';
             document.getElementById('rank').innerHTML = '<br><span class="red">NÃO LOGADO!</span>';
